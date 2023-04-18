a0a1c837a9d9df76b349765c62909a14de29aa27
rohan-201 <rohanpoddar780@gmail.com>
2023-04-18 13:01:29 +0530

    increase container width

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => (
             className="w-[24px] h-[24px] object-contain"
           />
           <span className="font-normal text-[16px] text-white">
-            Enter Metaverse
+            Join us for 50th Anniversary
           </span>
         </button>
       </div>
@@ -36,10 +36,10 @@ const Footer = () => (
 
         <div className="flex items-center justify-between flex-wrap gap-4">
           <h4 className="font-extrabold text-[24px] text-white">
-            METAVERUS
+            UmaVeena
           </h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2023 - 2024 UmaVeena. All rights reserved.
           </p>
 
           <div className="flex gap-4">
