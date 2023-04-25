05f45f133c78f55e4c3488674042f6b0cff03422
rohan-201 <rohanpoddar780@gmail.com>
2023-04-25 10:27:06 +0530

    addedd components

--- constants/index.js
+++ constants/index.js
@@ -50,21 +50,21 @@ export const newFeatures = [
 export const insights = [
   {
     imgUrl: '/a.JPG',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    title: 'A young maiden.',
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+        '',
   },
   {
     imgUrl: '/b.JPG',
-    title: '7 tips to easily master the madness of the Metaverse',
+    title: 'A handsome gentleman.',
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+        '',
   },
   {
     imgUrl: '/g.JPG',
-    title: 'With one platform you can explore the whole world virtually',
+    title: 'When they came together, destiny changed, new lives were born, a love saga that continues till date.',
     subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+        '',
   },
 ];
 
