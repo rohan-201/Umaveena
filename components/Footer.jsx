2f986d1c8e4f08a922c79f080aca086c5157c9bd
rohan-201 <rohanpoddar780@gmail.com>
2023-04-26 09:15:26 +0530

    addedd components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,16 +19,7 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
         Join us for 50th Anniversary
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-          <img
-            src="/headset.svg"
-            alt="headset"
-            className="w-[24px] h-[24px] object-contain"
-          />
-          <span className="font-normal text-[16px] text-white">
-            Talk to us
-          </span>
-        </button>
+        
         <a href="">
         <button  type="button"  className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
           <img
